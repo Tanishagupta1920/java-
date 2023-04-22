8d9407aec2c03cd98b60582d47c8d9c117cbbf66
Tanisha gupta <104975516+Tanishagupta1920@users.noreply.github.com>
2023-04-22 23:50:43 +0530

    Add files via upload

---
+++ Noargconstructor.java
@@ -0,0 +1,10 @@
+public class Noargconstructor {
+Noargconstructor(){
+System.out.println("no arg");
+}
+    public static void main(String[] args) {
+        Noargconstructor t=new Noargconstructor();
+
+    }
+    
+}
