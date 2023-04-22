3b89a473e8d304fc34391ee4ca87c418f4a54fcd
Tanisha gupta <104975516+Tanishagupta1920@users.noreply.github.com>
2023-04-22 23:49:53 +0530

    Add files via upload

---
+++ Defaultconsructor.java
@@ -0,0 +1,11 @@
+public class Defaultconsructor {
+    int i;
+    public static void main(String[] args) 
+        {
+             
+        Defaultconsructor  t =new Defaultconsructor();
+    
+            System.out.println(t.i);
+        }
+    
+}
