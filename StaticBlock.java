bd1979acfb8d46228b3cb4f50cee026fbdc76a6e
Tanisha gupta <104975516+Tanishagupta1920@users.noreply.github.com>
2023-04-28 23:18:01 +0530

    Add files via upload

---
+++ StaticBlock.java
@@ -0,0 +1,8 @@
+public class StaticBlock {
+    static{
+        System.out.println("Static block");
+    }public static void main(String[] args) {
+        System.out.println("Main Method");
+    }
+    
+}
