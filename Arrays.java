2c666b47b0aec5f8ef08cacb81950d91086f9899
Tanisha gupta <104975516+Tanishagupta1920@users.noreply.github.com>
2023-05-01 22:27:39 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,10 @@
+package Data;
+public class Arrays {
+
+    public static void main(String[] args) {
+        int[] a={10,20,30};
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
