6661bebefeabfa267378d64a565e044654a469c4
Tanisha gupta <104975516+Tanishagupta1920@users.noreply.github.com>
2023-05-05 23:04:55 +0530

    Add files via upload

---
+++ VectorArray.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class VectorArray{
+    public static void main(String[] args) {
+        Vector <Integer> v=new Vector<Integer>();
+        v.add(10);
+        v.add(20);
+        v.add(30);
+        v.add(40);
+        v.add(50);
+        System.out.println("Vector is:"+v);
+
+
+        
+
+    }
+}
