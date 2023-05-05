58080d515d14d518262c1f159d05203a11b609ae
Tanisha gupta <104975516+Tanishagupta1920@users.noreply.github.com>
2023-05-05 23:12:56 +0530

    Add files via upload

---
+++ Arraylist.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Arraylist {
+    public static void main(String[] args) {
+        int n=5;
+    
+        ArrayList<Integer> arr=new ArrayList<Integer>();
+        for(int i=0;i<n;i++){
+            System.out.print(i+" ");
+        }
+    }
+    
+}
