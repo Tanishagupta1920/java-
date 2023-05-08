04514d19c2d1af3d0d4ab0e5abc88408e1182b22
Tanisha gupta <104975516+Tanishagupta1920@users.noreply.github.com>
2023-05-08 23:47:32 +0530

    Add files via upload

---
+++ SecondSmallest.java
@@ -0,0 +1,17 @@
+public class SecondSmallest {
+    public static void main(String[] args) {
+        int[] a={7,9,5,2,4,6,8};
+        int temp;
+        for(int i=0;i<a.length;i++){
+            for(int j=i+1;j<a.length;j++){
+                if(a[i]>a[j]){
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+
+            }
+        }
+        System.out.println("Second Smallest element is: " +a[1]);
+    }
+}
