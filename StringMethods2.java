b8923b99d8795136044b519a68a608ba3ab7316b
Tanisha gupta <104975516+Tanishagupta1920@users.noreply.github.com>
2023-05-11 23:35:52 +0530

    Add files via upload

---
+++ StringMethods2.java
@@ -0,0 +1,12 @@
+public class StringMethods2 {
+    public static void main(String[] args) {
+        String s1="Deepak";
+        String s2="deepak@1234.gmail.com";
+        int i=s1.length();
+        if(i==0){
+            System.out.println("Name is empty");
+        }else{
+            System.out.println("Name is not empty");
+        }
+    }
+}
