53dea7620ea604bdc22a4ff0f81fedcb6c65e45f
Tanisha gupta <104975516+Tanishagupta1920@users.noreply.github.com>
2023-05-11 23:29:49 +0530

    Add files via upload

---
+++ StringMethods.java
@@ -0,0 +1,17 @@
+public class StringMethods {
+    public static void main(String[] args) {
+            String s1="Deepak";
+            String s2="deepak";
+            String s3="Sandhya";
+            System.out.println(s1.concat(s2));
+            System.out.println(s1+s3);
+            System.out.println(20+s1+10);
+            System.out.println(s1+10+20);
+            System.out.println(s1+10/5);
+            System.out.println(10+20+s1);
+
+    
+        
+    }
+    
+}
