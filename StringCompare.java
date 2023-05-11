5892a8ee97f135fff526a1efeee88fb69e80836a
Tanisha gupta <104975516+Tanishagupta1920@users.noreply.github.com>
2023-05-11 23:17:39 +0530

    Add files via upload

---
+++ StringCompare.java
@@ -0,0 +1,12 @@
+public class StringCompare {
+    public static void main(String[] args) {
+        String s1="Deepak";
+        String s2="deepak";
+        String s3="Sandhya";
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s1.compareToIgnoreCase(s2));
+
+    }
+    
+}
