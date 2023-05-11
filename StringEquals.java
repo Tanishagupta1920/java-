f3c9767c5f392be0cc82559e9b01b184a123855a
Tanisha gupta <104975516+Tanishagupta1920@users.noreply.github.com>
2023-05-11 23:07:00 +0530

    Add files via upload

---
+++ StringEquals.java
@@ -0,0 +1,12 @@
+public class StringEquals {
+    public static void main(String[] args) {
+        String s1="Deepak";
+        String s2="deepak";
+        String s3="Sandhya";
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+        System.out.println(s1.equalsIgnoreCase(s2));
+
+    }
+    
+}
